beedf2df3dec11376029b0519ec65bb815ae32ee
thecktoy <keulensilva0@gmail.com>
2021-10-30 22:40:46 -0300

    Crud Scaffolding

---
+++ ProjetoTesteAspNet/Data/ProjetoTesteAspNetContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoTesteAspNet.Models;
+
+namespace ProjetoTesteAspNet.Data
+{
+    public class ProjetoTesteAspNetContext : DbContext
+    {
+        public ProjetoTesteAspNetContext (DbContextOptions<ProjetoTesteAspNetContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoTesteAspNet.Models.Department> Department { get; set; }
+    }
+}
--- ProjetoTesteAspNet/Program.cs
+++ ProjetoTesteAspNet/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProjetoTesteAspNet.Data;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ProjetoTesteAspNetContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ProjetoTesteAspNetContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
